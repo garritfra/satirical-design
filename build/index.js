5cf585ec77f3567ff3648dd5f252808fd71315c6
Franke, Garrit <garrit.franke@ckc.de>
2018-04-25 15:05:32 +0200

    new boilerplate

--- build/index.js
+++ build/index.js
@@ -1,11 +1,15 @@
-"use strict";
+'use strict';
 
-var _Button = require("./components/Button");
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _Button2 = _interopRequireDefault(_Button);
+var _blocks = require('./blocks');
+
+var _blocks2 = _interopRequireDefault(_blocks);
+
+var _elements = require('./elements');
+
+var _elements2 = _interopRequireDefault(_elements);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = {
-    Button: _Button2.default
-};
+module.exports = _extends({}, _blocks2.default, _elements2.default);
